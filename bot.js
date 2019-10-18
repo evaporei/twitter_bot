2559565b0b668cbfa1d4b7e69103a94f4304abf2
0liveira <trueoliveir4@gmail.com>
2019-10-18 16:59:35 -0300

    chore: remove double quotes

--- bot.js
+++ bot.js
@@ -1,42 +1,42 @@
-"use strict"
+'use strict'
 
-console.log("The bot is starting")
+console.log('The bot is starting')
 
 //Twit package that handles the Twitter API
-let Twit = require("twit")
+let Twit = require('twit')
 
 //config keys
-let config = require("./config")
+let config = require('./config')
 let T = new Twit(config)
-let stream = T.stream("user")
+let stream = T.stream('user')
 
-stream.on("tweet", tweetEvent)
+stream.on('tweet', tweetEvent)
 
-let lastTweetPerson = ""
-let lastTweetBot = ""
+let lastTweetPerson = ''
+let lastTweetBot = ''
 
 function tweetEvent(eventMsg) {
 	let reply = eventMsg.in_reply_to_screen_name
 	let text = eventMsg.text
 	let userFrom = eventMsg.user.screen_name
 
-	if (reply === "DoMyMathBot") {
+	if (reply === 'DoMyMathBot') {
 		//this is actually blocked by Twitter, but double checking is good
 		if (text === lastTweetPerson) {
-			postTweet("You already sent that one!")
+			postTweet('You already sent that one!')
 			return
 		}
 
 		if (!text.match(/\d+/)) {
-      postTweet("I am sorry @" + userFrom + ", but this tweet does not contain numbers :(")
+      postTweet('I am sorry @' + userFrom + ', but this tweet does not contain numbers :(')
 		} else {
-			text = text.replace("@DoMyMathBot", "")
+			text = text.replace('@DoMyMathBot', ')
 
 			try {
 				let result = eval(text)//can do errors, so it will go to the catch part
-				postTweet("Hi, @" + userFrom + "! Here is your result ", result)
+				postTweet('Hi, @' + userFrom + '! Here is your result ', result)
 			} catch (error) {
-				postTweet("Hi, @" + userFrom + "! Can you check the expression? Look at my profile to see how to get a result")
+				postTweet('Hi, @' + userFrom + '! Can you check the expression? Look at my profile to see how to get a result')
 			}
     }
 
@@ -51,7 +51,7 @@ function postTweet(tweetText, res) {
 	//the same thing over and over again
 	if (tweetText === lastTweetBot) {
 		n++
-		tweetText = "The result was the same as the last one(" + res + "). Times it happened: " + n
+		tweetText = 'The result was the same as the last one(' + res + '). Times it happened: ' + n
 	} else {
 		tweetText = tweetText + res
 	}
@@ -62,11 +62,11 @@ function postTweet(tweetText, res) {
 
 	let gotData = (error, data, response) => {
 		if (error) {
-			console.log("Something went wrong! :(")
+			console.log('Something went wrong! :(')
 			console.log(error)
 		} else {
 			console.log(data.text)
 		}
 	}
-	T.post("statuses/update", tweet, gotData)
+	T.post('statuses/update', tweet, gotData)
 }
