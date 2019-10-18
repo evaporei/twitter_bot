b246695ea49c6ed3180e6925bb7e0f8392148262
0liveira <trueoliveir4@gmail.com>
2019-10-18 16:57:40 -0300

    chore: remove whitelines

--- bot.js
+++ bot.js
@@ -7,24 +7,20 @@ let Twit = require("twit")
 
 //config keys
 let config = require("./config")
-
 let T = new Twit(config)
-
 let stream = T.stream("user")
 
 stream.on("tweet", tweetEvent)
 
-let lastTweetPerson = "",
-	lastTweetBot = ""
+let lastTweetPerson = ""
+let lastTweetBot = ""
 
 function tweetEvent(eventMsg) {
-
 	let reply = eventMsg.in_reply_to_screen_name
 	let text = eventMsg.text
 	let userFrom = eventMsg.user.screen_name
-	
-	if (reply === "DoMyMathBot") {
 
+	if (reply === "DoMyMathBot") {
 		//this is actually blocked by Twitter, but double checking is good
 		if (text === lastTweetPerson) {
 			postTweet("You already sent that one!")
@@ -32,11 +28,8 @@ function tweetEvent(eventMsg) {
 		}
 
 		if (!text.match(/\d+/)) {
-
-	    	postTweet("I am sorry @" + userFrom + ", but this tweet does not contain numbers :(")
-
+      postTweet("I am sorry @" + userFrom + ", but this tweet does not contain numbers :(")
 		} else {
-
 			text = text.replace("@DoMyMathBot", "")
 
 			try {
@@ -45,32 +38,24 @@ function tweetEvent(eventMsg) {
 			} catch (error) {
 				postTweet("Hi, @" + userFrom + "! Can you check the expression? Look at my profile to see how to get a result")
 			}
-			
-		}
+    }
 
 		lastTweetPerson = text
-
 	}
-	
 }
 
 let n = 0
 
 function postTweet(tweetText, res) {
-
 	//this checks if the bot will not tweet
 	//the same thing over and over again
 	if (tweetText === lastTweetBot) {
-		
 		n++
 		tweetText = "The result was the same as the last one(" + res + "). Times it happened: " + n
-
 	} else {
-
 		tweetText = tweetText + res
-
 	}
-	
+
 	let tweet = {
 		status: tweetText
 	}
@@ -83,7 +68,5 @@ function postTweet(tweetText, res) {
 			console.log(data.text)
 		}
 	}
-
 	T.post("statuses/update", tweet, gotData)
-	
 }
